18c2729cb32f0bdbf22d8cabc3415a40a4df99fe
lminervino18 <lminervino@fi.uba.ar>
2025-01-16 16:36:20 -0300

    fix: backend endpoints are now accessible

---
+++ backend/src/main/java/com/mi_coleccion_camisetas/BackendColeccionCamisetasApplication.java
@@ -0,0 +1,12 @@
+package com.mi_coleccion_camisetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication // Escanea automáticamente todos los subpaquetes de com.mi_coleccion_camisetas
+public class BackendColeccionCamisetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendColeccionCamisetasApplication.class, args);
+	}
+}
