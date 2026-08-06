9a4e0380dd25b31b1bbe93db8304cfde8d1dea18
lminervino18 <lminervino@fi.uba.ar>
2026-08-06 19:40:48 -0300

    feat(collection): add sql-computed statistics and revocable share links

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,7 @@ export * from './accounts';
 export * from './errors';
 export * from './passwords';
 export * from './sessions';
+export * from './share-links';
 export * from './shirts';
+export * from './statistics';
 export * from './tokens';
